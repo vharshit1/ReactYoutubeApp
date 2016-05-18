f3363af8db87f54401a33c037434fd9598af9828
vharshit1 <vora.87@gmail.com>
2016-05-18 09:16:00 -0700

    Add files via upload

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDom from 'react-dom';
 import SearchBar from './components/search_bar';
-const App = ()=>{
-  return <div>
-  <SearchBar />
-   </div>;
-}
-
-ReactDom.render(<App/>,document.querySelector('.container'));
-
-
+import YTSearch from 'youtube-api-search'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyCL15S6qpmjsUfbbz8lUuM8UXI2DL1cV_c';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state= {videos:[]};
+
+    YTSearch({key:API_KEY,term: 'surfboards'},(videos)=>{
+      this.setState({videos})
+    });
+  }
+
+  render(){ return (<div>
+    <SearchBar />
+      <VideoDetail video={this.state.videos[0]} />
+      <VideoList videos={this.state.videos}/>
+
+    </div>);}
+  }
+
+  ReactDom.render(<App/>,document.querySelector('.container'));
