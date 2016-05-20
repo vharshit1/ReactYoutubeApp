e792f8e6f25cda175cdb709be1851f363b34b2e9
vharshit1 <vora.87@gmail.com>
2016-05-20 06:57:13 -0700

    Add files via upload

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -4,23 +4,41 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search'
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail'
+import _ from 'lodash';
 const API_KEY = 'AIzaSyCL15S6qpmjsUfbbz8lUuM8UXI2DL1cV_c';
 
 class App extends Component {
   constructor(props){
     super(props);
 
-    this.state= {videos:[]};
+    this.state= {
+      videos:[],
+    selectedVideo:null
+  };
+this.videoSearch('grand canyon 4k');
 
-    YTSearch({key:API_KEY,term: 'surfboards'},(videos)=>{
-      this.setState({videos})
-    });
   }
 
-  render(){ return (<div>
-    <SearchBar />
-      <VideoDetail video={this.state.videos[0]} />
-      <VideoList videos={this.state.videos}/>
+  videoSearch(term){
+    YTSearch({key:API_KEY,term:term},(videos)=>{
+      this.setState({
+        videos:videos,
+        selectedVideo:videos[0]
+
+      })
+    });
+
+  }
+
+  render(){
+
+const videoSearch= _.debounce((term)=> {this.videoSearch(term)},300)
+    return (<div>
+    <SearchBar onSearchTermChange={videoSearch}/>
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+      onVideoSelect= {selectedVideo=>this.setState({selectedVideo})}
+      videos={this.state.videos}/>
 
     </div>);}
   }
